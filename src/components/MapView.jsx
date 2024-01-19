9ef4326db1d8c615d8e92112af3160d575827e83
Edison Muñoz <edison.munoz1901@alumnos.ubiobio.cl>
2024-01-19 03:58:37 -0300

    v0.1/19-01

---
+++ src/components/MapView.jsx
@@ -0,0 +1,119 @@
+import React, { useEffect, useRef } from "react";
+import dynamic from "next/dynamic";
+import "leaflet/dist/leaflet.css";
+import "leaflet-geosearch/dist/geosearch.css";
+import "leaflet/dist/images/marker-icon.png";
+import "leaflet/dist/images/marker-shadow.png";
+
+const Map = dynamic(() => import("react-leaflet").then((mod) => mod.Map), {
+  ssr: false,
+});
+
+const TileLayer = dynamic(() => import("react-leaflet").then((mod) => mod.TileLayer), {
+  ssr: false,
+});
+
+const MapView = ({ onMapClick }) => {
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const L = require("leaflet");
+
+    if (!mapRef.current) {
+      mapRef.current = L.map("map", {
+        center: [-36.8225012, -73.0132711],
+        zoom: 10,
+        minZoom: 2,
+        maxZoom: 17,
+      });
+
+      const Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+      });
+
+      mapRef.current.addLayer(Esri_WorldStreetMap);
+
+      const LeafletGeosearch = require("leaflet-geosearch");
+
+      const searchControl = new LeafletGeosearch.GeoSearchControl({
+        provider: new LeafletGeosearch.OpenStreetMapProvider(),
+        style: 'bar',
+        showMarker: false,
+        showPopup: false,
+        autoClose: true,
+        retainZoomLevel: false,
+        animateZoom: true,
+        searchLabel: 'Ingrese dirección',
+      });
+
+      mapRef.current.addControl(searchControl);
+
+      mapRef.current.on("click", handleMapClick);
+
+      mapRef.current.on("move", () => {
+        const bounds = mapRef.current.getBounds();
+        const maxLat = 85.0511;
+        const minLat = -85.0511;
+
+        if (bounds.getNorth() > maxLat) {
+          mapRef.current.panBy([0, bounds.getNorth() - maxLat], { animate: false });
+        } else if (bounds.getSouth() < minLat) {
+          mapRef.current.panBy([0, bounds.getSouth() - minLat], { animate: false });
+        }
+      });
+    }
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove();
+        mapRef.current = null;
+      }
+    };
+  }, []);
+
+
+
+
+  let currentMarker = null;
+  const handleMapClick = (e) => {
+    const { lat, lng } = e.latlng;
+    console.log(`Latitud: ${lat}, Longitud: ${lng}`);
+    if (currentMarker) {
+      mapRef.current.removeLayer(currentMarker);
+    }
+
+    if (onMapClick) {
+      onMapClick(lat, lng);
+      const customIcon = L.icon({
+        iconUrl: 'marcador.png',
+        iconSize: [16, 32], 
+        iconAnchor: [16, 16], 
+        popupAnchor: [0, -32] 
+      });
+      currentMarker = L.marker([lat, lng], { icon: customIcon }).addTo(mapRef.current);
+    }
+
+
+    
+      
+  };
+
+
+  
+
+  return (
+    <div id="map" style={{ height: "100vh", width: "100%" }}>
+      <Map center={[-36.8225012, -73.0132711]} zoom={13} minZoom={5} maxZoom={15}>
+        <TileLayer
+          url="https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png"
+          attribution='<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>'
+          wrap={true}
+        />
+      </Map>
+ 
+    </div>
+    
+  );
+};
+
+export default MapView;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,198 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react';
+import MapView from '@/components/MapView';
+import axios from 'axios';
+import { saveAs } from 'file-saver';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [parameters, setParameters] = useState('');
+  const [comunidad, setComunidad] = useState('');
+  const [formato, setFormato] = useState('');
+  const [intervalo, setIntervalo] = useState('');
+
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(startDate, "fecha inicio")
+    console.log(endDate, "fecha termino")
+    console.log(latitude, "latitud")
+    console.log(longitude, "longitud")
+    console.log(parameters, "parametros")
+    console.log(comunidad, "comunidad")
+    console.log(formato, "formato")
+    console.log(intervalo, "intervalo")
+
+
+    hourlyApi();
+  };
+
+
+  const formatearFecha = (fecha) => {
+    const año = fecha.getFullYear();
+    const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+    const dia = fecha.getDate().toString().padStart(2, '0');
+    return `${año}${mes}${dia}`;
+  };
+
+
+  const hourlyApi = async () => {
+
+    const fechaInicioFormateada = formatearFecha(new Date(startDate));
+    const fechaFinFormateada = formatearFecha(new Date(endDate));
+    console.log(fechaInicioFormateada, "fecha inicio formateada")
+    console.log(fechaFinFormateada, "fecha fin formateada")
+    console.log(latitude, "latitud2")
+    console.log(longitude, "longitud2")
+    console.log(parameters, "parametros2")
+    console.log(comunidad, "comunidad2")
+    console.log(formato, "formato2")
+
+
+    try {
+
+      const response = await axios.get(`https://power.larc.nasa.gov/api/temporal/${intervalo}/point?start=${fechaInicioFormateada}&end=${fechaFinFormateada}&latitude=${latitude}&longitude=${longitude}&community=${comunidad}&parameters=${parameters}&format=${formato}&header=true&time-standard=lst`,
+        { responseType: 'blob' }
+      );
+      //'https://power.larc.nasa.gov/api/temporal/daily/point?start=${startDate}&end=${endDate}&latitude=${latitude}&longitude=${longitude}&community=${comunidad}&parameters=${parameters}&format=${formato}&header=true&time-standard=lst'
+
+      // Obtén la extensión del archivo según el formato seleccionado
+      let extension = '.nc'; // Valor predeterminado
+
+      switch (formato) {
+        case 'ascii':
+          extension = '.txt';
+          break;
+        case 'csv':
+          extension = '.csv';
+          break;
+        case 'json':
+          extension = '.json';
+          break;
+        // Puedes agregar más casos según sea necesari
+
+        default:
+          break;
+      }
+
+
+      // Obtén el nombre del archivo de la respuesta o utiliza uno predeterminado
+      const contentDisposition = response.headers['content-disposition'];
+      const fileNameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+      const matches = fileNameRegex.exec(contentDisposition);
+      const fileName = matches && matches[1] ? matches[1] : `archivo_descargado${extension}`;
+
+      // Guarda el archivo con la extensión correspondiente usando FileSaver.js
+      saveAs(response.data, fileName);
+
+      console.log(response.data, "data")
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+
+  const handleMapClick = (lat, lng) => {
+    setLatitude(lat);
+    //console.log(lat, "latitud")
+    setLongitude(lng);
+    //console.log(lng, "longitud")
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className='flex'>
+
+      <div className='bg-blue-100 flex items-center p-4'>
+        <form onSubmit={handleFormSubmit}>
+          <div className="flex mb-4 gap-x-2">
+            <label className='text-nowrap w-56 text-end'>Fecha de inicio</label>
+            <input type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} className="rounded-lg text-center w-full font-light" />
+          </div>
+
+          <div className="flex mb-4 gap-x-2">
+            <label className='text-nowrap w-56 text-end'>Fecha de término</label>
+            <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)}
+              className="rounded-lg text-center w-full font-thin" />
+          </div>
+
+          <div className="flex mb-4 gap-x-2">
+            <label className='text-nowrap w-56 text-end'>Latitud</label>
+            <input type="text" placeholder="Ingresa latitud" value={latitude} onChange={(e) => setLatitude(e.target.value)}
+              className="rounded-lg w-full text-center font-thin" />
+          </div>
+
+          <div className="flex mb-4 gap-x-2">
+            <label className="text-nowrap w-56 text-end">Longitud</label>
+            <input placeholder='Ingresa longitud' type="text" value={longitude} onChange={(e) => setLongitude(e.target.value)}
+              className="rounded-lg text-center w-full font-thin" />
+          </div>
+
+          <div className="flex mb-4 gap-x-2">
+            <label className="text-nowrap w-56 text-end">Comunidad</label>
+            <select className="rounded-lg text-center w-full font-thin"
+              onChange={(e) => setComunidad(e.target.value)}
+            >
+              <option value="" disabled selected hidden>Selecciona comunidad</option>
+              <option value="ag">Agroclimatología</option>
+              <option value="sb">Energía renovable</option>
+              <option value="re">Edificios Sostenibles</option>
+            </select>
+          </div>
+          <div className="flex mb-4 gap-x-2">
+            <label className="text-nowrap w-56 text-end">Intervalo</label>
+            <select className="rounded-lg text-center w-full font-thin"
+              onChange={(e) => setIntervalo(e.target.value)}
+            >
+              <option value="" disabled selected hidden>Selecciona intervalo </option>
+              <option value="hourly">Por Hora</option>
+              <option value="daily">Diario</option>
+              {/* <option value="monthly">Mensual y Anual</option>*/}
+            </select>
+          </div>
+
+          <div className="flex mb-4 gap-x-2">
+            <label className="text-nowrap w-56 text-end">Parámetros</label>
+            <select className="rounded-lg text-center w-full"
+              onChange={(e) => setParameters(e.target.value)}
+            >
+              <option value="" disabled selected hidden>Selecciona parametros</option>
+              <option value="T2M,T2MDEW,TS,T2MWET">Temperatura</option>
+              <option value="PRECTOT,QV2M,RH2M">Humedad/Precipitacion</option>
+              <option value="PS,WD10M,WS10M,WS50M,WD50M">Viento/Presion</option>
+            </select>
+          </div>
+
+          <div className="flex mb-4 gap-x-2">
+            <label className="text-nowrap w-56 text-end">Formato</label>
+            <select className="rounded-lg text-center w-full"
+              onChange={(e) => setFormato(e.target.value)}
+            >
+              <option value="" disabled selected hidden>Selecciona formato</option>
+              <option value="ascii">ASCII</option>
+              <option value="csv">CSV</option>
+              <option value="json">GeoJSON</option>
+              <option value="netcdf">NetCDF</option>
+            </select>
+
+          </div>
+          <button type="submit" className="bg-blue-500 block w-full text-white px-4 py-2 rounded">
+            Enviar
+          </button>
+        </form>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='w-full'>
+        <MapView onMapClick={handleMapClick} />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  );
 }
