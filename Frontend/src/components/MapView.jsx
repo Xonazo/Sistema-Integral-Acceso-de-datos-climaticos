7863c938ecbddd9fefdcbaf79926559089e3d32f
Edison Muñoz <edison.munoz1901@alumnos.ubiobio.cl>
2024-04-20 20:25:44 -0400

    20-04/draw in map

--- Frontend/src/components/MapView.jsx
+++ Frontend/src/components/MapView.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
-import "leaflet-geosearch/dist/geosearch.css";
 import "leaflet/dist/images/marker-icon.png";
 import "leaflet/dist/images/marker-shadow.png";
 
@@ -13,93 +12,129 @@ const TileLayer = dynamic(() => import("react-leaflet").then((mod) => mod.TileLa
   ssr: false,
 });
 
-const MapView = ({ onMapClick }) => {
+const MapView = ({ onMapClick , onRectangle}) => {
   const mapRef = useRef(null);
+  const handleRef = useRef(null);
+  const rectangleRef = useRef(null);
 
   useEffect(() => {
-    const L = require("leaflet");
+    if (typeof window !== "undefined") {
+      const L = require("leaflet");
+      require("leaflet-path-transform");
+      if (!mapRef.current) {
+        mapRef.current = L.map("map", {
+          center: [-36.8225012, -73.0132711],
+          zoom: 10,
+          minZoom: 2,
+          maxZoom: 17,
+        });
 
-    if (!mapRef.current) {
-      mapRef.current = L.map("map", {
-        center: [-36.8225012, -73.0132711],
-        zoom: 10,
-        minZoom: 2,
-        maxZoom: 17,
-      });
+        const Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+          attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+        });
 
-      const Esri_WorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
-      });
+        mapRef.current.addLayer(Esri_WorldStreetMap);
 
-      mapRef.current.addLayer(Esri_WorldStreetMap);
+        const LeafletGeosearch = require("leaflet-geosearch");
 
-      const LeafletGeosearch = require("leaflet-geosearch");
+        const searchControl = new LeafletGeosearch.GeoSearchControl({
+          provider: new LeafletGeosearch.OpenStreetMapProvider(),
+          style: 'bar',
+          showMarker: false,
+          showPopup: false,
+          autoClose: true,
+          retainZoomLevel: false,
+          animateZoom: true,
+          searchLabel: 'Ingrese dirección',
+        });
 
-      const searchControl = new LeafletGeosearch.GeoSearchControl({
-        provider: new LeafletGeosearch.OpenStreetMapProvider(),
-        style: 'bar',
-        showMarker: false,
-        showPopup: false,
-        autoClose: true,
-        retainZoomLevel: false,
-        animateZoom: true,
-        searchLabel: 'Ingrese dirección',
-      });
+        mapRef.current.addControl(searchControl);
 
-      mapRef.current.addControl(searchControl);
+        mapRef.current.on("click", handleMapClick);
 
-      mapRef.current.on("click", handleMapClick);
+        mapRef.current.on("move", () => {
+          const bounds = mapRef.current.getBounds();
+          const maxLat = 85.0511;
+          const minLat = -85.0511;
 
-      mapRef.current.on("move", () => {
-        const bounds = mapRef.current.getBounds();
-        const maxLat = 85.0511;
-        const minLat = -85.0511;
-
-        if (bounds.getNorth() > maxLat) {
-          mapRef.current.panBy([0, bounds.getNorth() - maxLat], { animate: false });
-        } else if (bounds.getSouth() < minLat) {
-          mapRef.current.panBy([0, bounds.getSouth() - minLat], { animate: false });
-        }
-      });
+          if (bounds.getNorth() > maxLat) {
+            mapRef.current.panBy([0, bounds.getNorth() - maxLat], { animate: false });
+          } else if (bounds.getSouth() < minLat) {
+            mapRef.current.panBy([0, bounds.getSouth() - minLat], { animate: false });
+          }
+        });
+      }
     }
 
     return () => {
       if (mapRef.current) {
         mapRef.current.remove();
-        mapRef.current = null; 
+        mapRef.current = null;
       }
     };
   }, []);
 
-
-
-
-  let currentMarker = null;
   const handleMapClick = (e) => {
     const { lat, lng } = e.latlng;
     console.log(`Latitud: ${lat}, Longitud: ${lng}`);
-    if (currentMarker) {
-      mapRef.current.removeLayer(currentMarker);
-    }
-
     if (onMapClick) {
       onMapClick(lat, lng);
-      const customIcon = L.icon({
-        iconUrl: 'marcador.png',
-        iconSize: [16, 32], 
-        iconAnchor: [16, 16], 
-        popupAnchor: [0, -32] 
-      });
-      currentMarker = L.marker([lat, lng], { icon: customIcon }).addTo(mapRef.current);
+      if (!rectangleRef.current) {
+        const bounds = [
+          [lat - 0.025, lng - 0.025],
+          [lat - 0.025, lng + 0.025],
+          [lat + 0.025, lng + 0.025],
+          [lat + 0.025, lng - 0.025],
+          [lat - 0.025, lng - 0.025]
+        ];
+        rectangleRef.current = L.polygon(bounds, { transform: true, color: "#ff7800", weight: 1 }).addTo(mapRef.current);
+
+        rectangleRef.current.transform.enable();
+
+        const southWest = rectangleRef.current.getBounds().getSouthWest();
+        const northEast = rectangleRef.current.getBounds().getNorthEast();
+        console.log("Latitud inferior izquierda:", southWest.lat, "Longitud inferior izquierda:", southWest.lng);
+        console.log("Latitud superior derecha:", northEast.lat, "Longitud superior derecha:", northEast.lng);
+
+        rectangleRef.current.on("transform", handleTransform);
+        onRectangle(southWest.lat, southWest.lng, northEast.lat, northEast.lng);
+      }
+
+      if (!handleRef.current) {
+        const handleBounds = [
+          [lat - 0.025, lng + 0.025],
+          [lat + 0.025, lng + 0.025],
+          [lat + 0.025, lng + 0.05],
+          [lat - 0.025, lng + 0.05],
+          [lat - 0.025, lng + 0.025]  // cerrando la zona de manejo
+        ]; // Ajusta los límites de la zona de manejo según tus necesidades
+        handleRef.current = L.polygon(handleBounds, { color: "#000", weight: 0 }).addTo(mapRef.current);
+      }
     }
-
-
-    
-      
   };
+  useEffect(() => {
+    if (rectangleRef.current) {
+      rectangleRef.current.on("transform", handleTransform);
+    }
+  }, [rectangleRef.current]); 
 
-
+  const handleTransform = () => {
+    const bounds = rectangleRef.current.getLatLngs()[0];
+    let bottomLeft, upperRight;
   
+    bounds.forEach((corner, index) => {
+      if (index === 0) {
+        bottomLeft = corner;
+        console.log(`Latitud inferior izquierda: ${corner.lat}, Longitud inferior izquierda: ${corner.lng}`);
+      }
+      if (index === 2) {
+        upperRight = corner;
+        console.log(`Latitud superior derecha: ${corner.lat}, Longitud superior derecha: ${corner.lng}`);
+      }
+    });
+  
+    onRectangle(bottomLeft.lat,  upperRight.lat ,bottomLeft.lng, upperRight.lng);
+  };
 
   return (
     <div id="map" style={{ height: "100vh", width: "100%" }}>
@@ -110,9 +145,7 @@ const MapView = ({ onMapClick }) => {
           wrap={true}
         />
       </Map>
- 
     </div>
-    
   );
 };
 
--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
   const [comunidad, setComunidad] = useState('');
   const [formato, setFormato] = useState('');
   const [intervalo, setIntervalo] = useState('');
-  
+
 
 
 
@@ -151,6 +151,139 @@ export default function Home() {
   const [activeTab, setActiveTab] = useState('tab1');
   const [activarBoton, setActivarBoton] = useState(false);
 
+
+
+
+
+
+  const [latitudInferiorIzquierda, setLatitudInferiorIzquierda] = useState(null);
+  const [longitudInferiorIzquierda, setLongitudInferiorIzquierda] = useState(null);
+  const [latitudSuperiorDerecha, setLatitudSuperiorDerecha] = useState(null);
+  const [longitudSuperiorDerecha, setLongitudSuperiorDerecha] = useState(null);
+
+  
+
+  const cords = (lat1, lat2, lng1, lng2) => {
+    setLatitudInferiorIzquierda(lat1);
+    setLongitudInferiorIzquierda(lng1);
+    setLatitudSuperiorDerecha(lat2);
+    setLongitudSuperiorDerecha(lng2);
+    
+  };
+
+
+
+
+
+  const formulario2 = () => {
+
+    return (
+      <form onSubmit={(e) => { e.preventDefault(); }}>
+        <div>
+          <h1 className="text-center text-2xl font-bold py-5">Zone</h1>
+        </div>
+        <div className="flex mb-4 gap-x-2">
+          <label className='text-nowrap w-56 text-end'>Fecha de inicio</label>
+          <input type="date" className="rounded-lg text-center w-full font-light" />
+        </div>
+
+        <div className="flex mb-4 gap-x-2">
+          <label className='text-nowrap w-56 text-end'>Fecha de término</label>
+          <input type="date"
+
+            className="rounded-lg text-center w-full font-thin" />
+        </div>
+
+        <div className="flex mb-4 gap-x-2">
+          <label className='text-nowrap w-56 text-end'>Latitud inferior izquierda:</label>
+          <input type="text" placeholder="Ingresa latitud"
+            onChange={(e) => setLatitudInferiorIzquierda(e.target.value)}
+            value={latitudInferiorIzquierda}
+            className="rounded-lg w-full text-center font-thin" />
+        </div>
+
+        <div className="flex mb-4 gap-x-2">
+          <label className="text-nowrap w-56 text-end">Longitud inferior izquierda:</label>
+          <input placeholder='Ingresa longitud' type="text"
+            onChange={(e) => setLongitudInferiorIzquierda(e.target.value)}
+            value={longitudInferiorIzquierda}
+            className="rounded-lg text-center w-full font-thin" />
+        </div>
+
+        <div className="flex mb-4 gap-x-2">
+          <label className='text-nowrap w-56 text-end'>Latitud superior derecha:</label>
+          <input type="text" placeholder="Ingresa latitud"
+            onChange={(e) => setLatitudSuperiorDerecha(e.target.value)}
+            value={latitudSuperiorDerecha}
+            className="rounded-lg w-full text-center font-thin" />
+        </div>
+
+        <div className="flex mb-4 gap-x-2">
+          <label className="text-nowrap w-56 text-end">Longitud superior derecha:</label>
+          <input placeholder='Ingresa longitud' type="text"
+            onChange={(e) => setLongitudSuperiorDerecha(e.target.value)}
+            value={longitudSuperiorDerecha}
+            className="rounded-lg text-center w-full font-thin" />
+        </div>
+
+
+        <div className="flex mb-4 gap-x-2">
+          <label className="text-nowrap w-56 text-end">Comunidad</label>
+          <select className="rounded-lg text-center w-full font-thin"
+
+          >
+            <option value="" disabled selected hidden>Selecciona comunidad</option>
+            <option value="ag">Agroclimatología</option>
+            <option value="sb">Energía renovable</option>
+            <option value="re">Edificios Sostenibles</option>
+          </select>
+        </div>
+        <div className="flex mb-4 gap-x-2">
+          <label className="text-nowrap w-56 text-end">Intervalo</label>
+          <select className="rounded-lg text-center w-full font-thin"
+
+          >
+            <option value="" disabled selected hidden>Selecciona intervalo </option>
+            <option value="hourly">Por Hora</option>
+            <option value="daily">Diario</option>
+          </select>
+        </div>
+
+        <div className="flex mb-4 gap-x-2">
+          <label className="text-nowrap w-56 text-end">Parámetros</label>
+          <select className="rounded-lg text-center w-full"
+
+          >
+            <option value="" disabled selected hidden>Selecciona parametros</option>
+            <option value="T2M,T2MDEW,TS,T2MWET">Temperatura</option>
+            <option value="PRECTOT,QV2M,RH2M">Humedad/Precipitacion</option>
+            <option value="PS,WD10M,WS10M,WS50M,WD50M">Viento/Presion</option>
+          </select>
+        </div>
+
+        <div className="flex mb-4 gap-x-2">
+          <label className="text-nowrap w-56 text-end">Formato</label>
+          <select className="rounded-lg text-center w-full"
+          >
+            <option value="" disabled selected hidden>Selecciona formato</option>
+            <option value="ascii">ASCII</option>
+            <option value="csv">CSV</option>
+            <option value="json">GeoJSON</option>
+            <option value="netcdf">NetCDF</option>
+          </select>
+        </div>
+
+        <button className="bg-blue-500 block w-full text-white px-4 py-2 rounded">
+          Enviar
+        </button>
+      </form>
+    )
+  }
+
+
+
+
+
   return (
 
     <>
@@ -263,108 +396,17 @@ export default function Home() {
                 </form>
               ) : (
                 // Formulario Tab 2 
-                <form onSubmit={handleFormSubmit}>
-                  <div>
-                    <h1 className="text-center text-2xl font-bold py-5">Zone</h1>
-                  </div>
-                  <div className="flex mb-4 gap-x-2">
-                    <label className='text-nowrap w-56 text-end'>Fecha de inicio</label>
-                    <input type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} className="rounded-lg text-center w-full font-light" />
-                  </div>
-
-                  <div className="flex mb-4 gap-x-2">
-                    <label className='text-nowrap w-56 text-end'>Fecha de término</label>
-                    <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)}
-                      className="rounded-lg text-center w-full font-thin" />
-                  </div>
-
-                  <div className="flex mb-4 gap-x-2">
-                    <label className='text-nowrap w-56 text-end'>Latitud 1</label>
-                    <input type="text" placeholder="Ingresa latitud" value={latitude} onChange={(e) => setLatitude(e.target.value)}
-                      className="rounded-lg w-full text-center font-thin" />
-                  </div>
-
-                  <div className="flex mb-4 gap-x-2">
-                    <label className="text-nowrap w-56 text-end">Longitud1</label>
-                    <input placeholder='Ingresa longitud' type="text" value={longitude} onChange={(e) => setLongitude(e.target.value)}
-                      className="rounded-lg text-center w-full font-thin" />
-                  </div>
-
-                  <div className="flex mb-4 gap-x-2">
-                    <label className='text-nowrap w-56 text-end'>Latitud 2</label>
-                    <input type="text" placeholder="Ingresa latitud" value={latitude} onChange={(e) => setLatitude(e.target.value)}
-                      className="rounded-lg w-full text-center font-thin" />
-                  </div>
-
-                  <div className="flex mb-4 gap-x-2">
-                    <label className="text-nowrap w-56 text-end">Longitud2</label>
-                    <input placeholder='Ingresa longitud' type="text" value={longitude} onChange={(e) => setLongitude(e.target.value)}
-                      className="rounded-lg text-center w-full font-thin" />
-                  </div>
-              
-
-                  <div className="flex mb-4 gap-x-2">
-                    <label className="text-nowrap w-56 text-end">Comunidad</label>
-                    <select className="rounded-lg text-center w-full font-thin"
-                      onChange={(e) => setComunidad(e.target.value)}
-                    >
-                      <option value="" disabled selected hidden>Selecciona comunidad</option>
-                      <option value="ag">Agroclimatología</option>
-                      <option value="sb">Energía renovable</option>
-                      <option value="re">Edificios Sostenibles</option>
-                    </select>
-                  </div>
-                  <div className="flex mb-4 gap-x-2">
-                    <label className="text-nowrap w-56 text-end">Intervalo</label>
-                    <select className="rounded-lg text-center w-full font-thin"
-                      onChange={(e) => setIntervalo(e.target.value)}
-                    >
-                      <option value="" disabled selected hidden>Selecciona intervalo </option>
-                      <option value="hourly">Por Hora</option>
-                      <option value="daily">Diario</option>
-
-                    </select>
-                  </div>
-
-                  <div className="flex mb-4 gap-x-2">
-                    <label className="text-nowrap w-56 text-end">Parámetros</label>
-                    <select className="rounded-lg text-center w-full"
-                      onChange={(e) => setParameters(e.target.value)}
-                    >
-                      <option value="" disabled selected hidden>Selecciona parametros</option>
-                      <option value="T2M,T2MDEW,TS,T2MWET">Temperatura</option>
-                      <option value="PRECTOT,QV2M,RH2M">Humedad/Precipitacion</option>
-                      <option value="PS,WD10M,WS10M,WS50M,WD50M">Viento/Presion</option>
-                    </select>
-                  </div>
-
-                  <div className="flex mb-4 gap-x-2">
-                    <label className="text-nowrap w-56 text-end">Formato</label>
-                    <select className="rounded-lg text-center w-full"
-                      onChange={(e) => setFormato(e.target.value)}
-                    >
-                      <option value="" disabled selected hidden>Selecciona formato</option>
-                      <option value="ascii">ASCII</option>
-                      <option value="csv">CSV</option>
-                      <option value="json">GeoJSON</option>
-                      <option value="netcdf">NetCDF</option>
-                    </select>
-
-                  </div>
-                  <button type="submit" className="bg-blue-500 block w-full text-white px-4 py-2 rounded">
-                    Enviar
-                  </button>
-                </form>
+                formulario2()
               )}
           </div>
           <div class="flex justify-center items-center " >
-          <a href="/graficos" class="text-blue-500 underline"> Graficar</a>
+            <a href="/graficos" class="text-blue-500 underline"> Graficar</a>
           </div>
         </div>
 
 
         <div className='w-full'>
-          <MapView onMapClick={handleMapClick} />
+          <MapView onMapClick={handleMapClick}  onRectangle={cords}/>
         </div>
       </div>
 
