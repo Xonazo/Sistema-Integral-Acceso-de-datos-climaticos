2c57b23eeea3396d3a19f60dc7df6992146aa802
Edison Muñoz <edison.munoz1901@alumnos.ubiobio.cl>
2024-08-07 00:50:50 -0400

    versionFinal

--- Backend/app.py
+++ Backend/app.py
@@ -11,7 +11,8 @@ from flask import Flask, request, jsonify, after_this_request
 from flask_cors import CORS  
 
 app = Flask(__name__)
-CORS(app)  
+
+CORS(app, resources={r"/upload": {"origins": "http://localhost:3000"}})
 
 # Ruta 
 @app.route('/upload', methods=['POST'])
